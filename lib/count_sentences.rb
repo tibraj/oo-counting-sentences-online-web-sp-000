0d875f63c7bf0692dee8c7866a40d80b16d84bfc
Taulant Ibraj <tibraj94@gmail.com>
2020-04-09 19:49:48 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,27 +3,15 @@ require 'pry'
 class String
 
   def sentence?
-    if self.end_with?(".")
-      return true
-    else
-      return false
-    end
+    self.end_with?(".") ? true : false
   end
 
   def question?
-    if self.end_with?("?")
-      return true
-    else
-      return false
-    end
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-    if self.end_with?("!")
-      return true
-    else
-      return false
-    end
+    self.end_with?("1") ? true : false      
   end
 
   def count_sentences
